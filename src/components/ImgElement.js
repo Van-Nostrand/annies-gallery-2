5412adbd3fe5a2e2136114ab9f1ad2c740047d5f
Van-Nostrand <dmdoull43@gmail.com>
2021-02-15 14:03:37 -0800

    quick commit before merge

---
+++ src/components/ImgElement.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ImgElement = (props) => {
+
+  let [ imagePath, setImagePath ] = useState();
+  let [ landingPage, setLandingPage ] = useState();
+  let current = useLocation();
+
+  const checkCurrent = () => {
+    setLandingPage(current.pathname === "/");
+  }
+
+  useEffect(() => {
+    checkCurrent();
+  });
+
+
+
+  useEffect(() => {
+    const context = require.context('../../assets', false, /.*(threehands).*(png|jpe?g)$/);
+    const images = context.keys().map(context);
+    setImagePath(images[0]);
+  },[]);
+
+  if(imagePath){
+    return (
+      <img className='background-img-element' src={imagePath} alt="berrypicture" />
+    )
+  }
+
+  //import image here
+  return(
+    <span className='background-img-ph'></span>
+  )
+}
+
+export default ImgElement;
